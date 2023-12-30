2c78c782291008bc7217f6722c92303fc570dd38
Zehra Gul Buyukarslan <151182584+zehragulbuyukarslan@users.noreply.github.com>
2023-12-30 23:27:01 +0300

    Add files via upload

---
+++ 04- print sum of two values.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int a,b;
+	printf("\nenter the first value:\n");
+	scanf("%d",&a);
+	printf("\nenter the second value:\n");
+	scanf("%d",&b);
+	printf("\nsum of values: %d", a+b);
+	
+}
