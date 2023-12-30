cfafafa1f808e1ade2ca0ae6fad760a511af8352
Zehra Gul Buyukarslan <151182584+zehragulbuyukarslan@users.noreply.github.com>
2023-12-30 23:26:17 +0300

    Add files via upload

---
+++ 02C- find age by checking birthdate.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int bd,year;
+	printf("\nenter your birthdate:\n");
+	year=2024;
+	scanf("%d", &bd);
+	printf("your age is: %d", year-bd);
+	
+}
