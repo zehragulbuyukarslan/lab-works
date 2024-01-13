98c3761121830f474242158e3ad1f908e6cfd9ca
Zehra Gul Buyukarslan <151182584+zehragulbuyukarslan@users.noreply.github.com>
2024-01-13 06:26:27 +0300

    Add files via upload

---
+++ 49C- Sort Strings in Dictionary Order.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char s[40], ss[400];
+	int i, k, ii=0;
+	
+	for(i=1; i<=10; i++){
+		gets(s);
+		for(k=0; s[k]!='\0' k++){
+			ss[ii]=s[k];
+			ii++;
+		}
+		ss[ii]=' ';
+	}
+	
+	for(k=0; ss[k]!='\0'; k++){
+	    
+	}
+	
+}
